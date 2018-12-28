7f274bb15a135491e764313277f7ad4d940ad897
eragonriddle <eragonriddle@gmail.com>
2018-12-28 21:55:33 +0000

    Added license and package script

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md". "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+        name="pycertverifier",
+        version="0.0.1",
+        author="Eragon Riddle",
+        author_email="eragonriddle@gmail.com",
+        description="A python package to check x509 certificate validity",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        url="https://github.com/eragonriddle/pycertverifier",
+        packages=setuptools.find_packages(),
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+        ],
+)
